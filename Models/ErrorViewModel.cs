4e3e6bc740df0191339139b83a69eadce1e422a8
nvduoc_dev <duoc01699232692@gmail.com>
2026-09-09 20:00:25 +0700

    ++

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SystemNewsManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
